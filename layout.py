997fa65f88acd73cc657a4e2d44b9a34390b0e63
Arbyys <adam.petricek@pslib.cz>
2020-12-02 15:23:25 +0100

    ignore

--- layout.py
+++ layout.py
@@ -1,5 +1,7 @@
-with open('input.txt') as file:
+with open('../input.txt') as file:
     data = file.read().split("\n")
-    data = data[:-1]
+    data = data[:-1] # remove last empty line test
+
+    data = list(map(int, data)) # to int
 
 print(data)
